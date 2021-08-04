081e9924b12637c34f95edffae33556299eaed71
Programistich <alexasda051@gmail.com>
2021-08-04 13:52:27 +0300

    add description models

--- src/models/User.ts
+++ src/models/User.ts
@@ -6,10 +6,10 @@ export interface User {
     auth: boolean, // Верификацтия через Вастрик Апи
     nickname: string, // Никнейм в тг? а если его нет
     dateRegistration: Date, // Дата регистарации
-    countReview: number, // Количество отзывов
-    countPlace: number, // Количество мест
-    countList: number, // Количество списков
-    listPlaceReview: Array<[Place, Review]> // Список мест + отзыв от человека
+    countReview: number, // Количество отзывов (если едитор)
+    countPlace: number, // Количество мест (если едитор)
+    countList: number, // Количество списков (если едитор)
+    listPlaceReview: Array<[Place, Review]> // Список мест + отзыв от человека (если едитор)
 }
 
 enum UserType {
